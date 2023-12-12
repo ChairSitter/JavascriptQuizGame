6e28861a55a1c918f36c62f2ffc94596c3a8a9f0
Bryan Schneller <bryan.schneller@gmail.com>
2023-12-12 17:41:21 -0500

    Add content of question array, test functionality of adding question object text to appropriate HTML elements

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ const answerD = document.querySelector("#answerD");
 const timer = document.querySelector("#timer");
 
 let time = 60;
+let roundNum = 0;
 
 const questionBank = [
     {
@@ -61,43 +62,106 @@ const questionBank = [
     },
     {
         name: "Question 3",
-        question: "What year was JavaScript invented?", 
-        A: "2015",
-        B: "1995", 
-        C: "1988", 
-        D: "1990",
+        question: "What method allows the developer to read a custom message in DevTools?", 
+        A: "console.log()",
+        B: "message.read()", 
+        C: "developer.text()", 
+        D: "script.java()",
+        correct: "A",
+    },
+    {
+        name: "Question 4",
+        question: "What is the official language name of JavaScript?", 
+        A: "PageScript",
+        B: "Doug's cool language", 
+        C: "Java", 
+        D: "ECMAScript",
+        correct: "D",
+    },
+    {
+        name: "Question 5",
+        question: "Which three are all data types?", 
+        A: "string, number, boolean",
+        B: "bingo, numeric, string", 
+        C: "boonsky, numrod, strang", 
+        D: "straang, noomber, billean",
+        correct: "A",
+    },
+    {
+        name: "Question 6",
+        question: "What does JavaScript contribute to a website?", 
+        A: "Content and order",
+        B: "Interactivity", 
+        C: "Styles and display rules", 
+        D: "JavaScript is the lazy moocher of the web",
         correct: "B",
     },
     {
-        name: "Question 1",
-        question: "What year was JavaScript invented?", 
-        A: "2015",
-        B: "1995", 
-        C: "1988", 
-        D: "1990",
-        correct: "B",
+        name: "Question 7",
+        question: "How do you write an arrow function?", 
+        A: "const functionName ==========> { ... }",
+        B: "const functionName (arrow) { ... }", 
+        C: "const arrow = arrowFunction (arrow) { I like arrows }", 
+        D: "const functionName = () => { ... }",
+        correct: "D",
+    },
+    {
+        name: "Question 8",
+        question: "Explain this: for(let i=0; i < array.length; i++) { ... }?", 
+        A: "Set i to zero; while i is less than the length of the array, run the code; then increment i by one.",
+        B: "Please allow i to be zero; point array.length at i; add two i's.", 
+        C: "Set i to zero; while i is greater than the length of the array, run the code; then increment i by one.", 
+        D: "Set i to zero; while i is less than the length of the array, increment i by one; then run the code.",
+        correct: "D",
+    },
+    {
+        name: "Question 9",
+        question: "What is a method?", 
+        A: "An interview technique involving answering questions with deep, rapid-fire personal questions",
+        B: "A variable with no objective value", 
+        C: "A function that is the value of an object's property", 
+        D: "Most of the characters from the hit TV series Breaking Bad",
+        correct: "C",
+    },
+    {
+        name: "Question 10",
+        question: "What is a class?", 
+        A: "A material used for making windows",
+        B: "A template for creating objects", 
+        C: "An object used for increased site security", 
+        D: "A local storage container",
+        correct: "D",
     }
 ]
 
-console.log(questionBank[0].B, questionBank[0].correct);
+const displayQuestion = () => {
+    question.textContent = `#${roundNum + 1}: ` + questionBank[roundNum].question;
+    answerA.textContent = "A: " + questionBank[roundNum].A;
+    answerB.textContent = "B: " + questionBank[roundNum].B;
+    answerC.textContent = "C: " + questionBank[roundNum].C;
+    answerD.textContent = "D: " + questionBank[roundNum].D;
+    roundNum++;
+}
 
 const controlTimer = () => {
     const decrementTime = () => {
         time--;
         timer.textContent = time; 
-        if(time === 0){
+        if(time <= 0){
             clearInterval(myInterval);
         }
     }
     let myInterval = setInterval(decrementTime, 1000);
-
 }
 
+const endGame = () => {
 
+}
 
 const game = (event) => {
     event.preventDefault();
     controlTimer();
+    displayQuestion();
     console.log("working");
 }
 
